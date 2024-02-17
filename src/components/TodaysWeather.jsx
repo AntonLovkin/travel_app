0f6232078a6e1ceab21635a594c883fd1f268118
AntonLovkin <anton28111987@gmail.com>
2024-02-17 23:28:10 +0200

    added today day of the week

--- src/components/TodaysWeather.jsx
+++ src/components/TodaysWeather.jsx
@@ -1,11 +1,14 @@
 import Timer from './Timer';
+import getDayOfWeek from '../getDayOfWeek';
 
 const TodaysWeather = ({data, date}) => {
     // console.log(date)
     const { datetime, temp, icon} = data;
+    const dayOfWeek = getDayOfWeek(datetime);
     return (
         <div style={{border: "2px solid white", borderRadius: "20px", padding: "10px", margin: "10px", cursor: "pointer", backgroundColor: "white"}}>
                 <h3>Todays Weather</h3>
+                <h3>{dayOfWeek}</h3>
                 <p>{datetime}</p>
                 <p>{temp} deg</p>
                 <p>{icon}</p>
