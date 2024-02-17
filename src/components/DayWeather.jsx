8f05646ae7db746bb4b6a11158e0f0c1f418f4f0
AntonLovkin <anton28111987@gmail.com>
2024-02-17 23:25:26 +0200

    added day of the week function

--- src/components/DayWeather.jsx
+++ src/components/DayWeather.jsx
@@ -1,8 +1,13 @@
+import getDayOfWeek from "../getDayOfWeek";
+
 const DayWeather = ({data}) => {
     const { datetime, temp, icon} = data;
+    const dayOfWeek = getDayOfWeek(datetime);
+
     return (
         <li style={{border: "2px solid white", borderRadius: "20px", padding: "10px", margin: "10px", cursor: "pointer"}}>
-                <h3>{datetime}</h3>
+                <h3>{dayOfWeek}</h3>
+                <p>{datetime}</p>
                 <p>{temp}</p>
                 <p>{icon}</p>
               </li>
