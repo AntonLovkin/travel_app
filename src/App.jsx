ac3c7502c9da0aabf6cfbaec4c23fb2c48d262c9
AntonLovkin <anton28111987@gmail.com>
2024-02-17 18:55:59 +0200

    added trip & wheater info components

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import viteLogo from '/vite.svg';
 import './App.css';
 
 import Modal from './components/Modal.jsx';
+import Trip from './components/Trip.jsx';
+import DayWeather from './components/DayWeather.jsx';
 
 const API_KEY = 'XM9YG9VGNME7QHFXDNCCMDYFU';
 const tripsList = [
   { city: 'Kyiv', startDate: '2024-03-06', endDate: '2024-03-09' },
-  { city: 'Lviv', startDate: '2024-03-06', endDate: '2024-03-09' },
+  { city: 'Tokio', startDate: '2024-03-06', endDate: '2024-03-09' },
 ];
 
 function App() {
@@ -18,6 +20,7 @@ function App() {
   const [tripsFromLocalstorage, setTripsFromLocalstorage] = useState(
     JSON.parse(localStorage.getItem('trips')) || tripsList || []
   );
+  const [tripWeatherInfo, setTripWeatherInfo] = useState(null);
 
   useEffect(() => {
     const trips = JSON.parse(localStorage.getItem('trips'));
@@ -73,6 +76,28 @@ function App() {
     console.log(tripsFromLocalstorage);
   };
 
+  const onTripClick = (city, startDate, endDate) => {
+    console.log(city, startDate, endDate);
+    console.log("click");
+    fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}/${startDate}/${endDate}?unitGroup=metric&include=days&key=${API_KEY}&contentType=json`
+    )
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data.days)
+        setTripWeatherInfo(data.days);
+      })
+      .catch((error) => {
+        // setInfo(`There is no such city like ${city}, try English.`);
+        // console.error('There was a problem with the request:', error);
+      });
+  };
+
   return (
     <>
       <div>
@@ -84,7 +109,7 @@ function App() {
       <div>
         <h2>List of Trips</h2>
         <ul>
-          {tripsFromLocalstorage.length > 0 &&
+          {/* {tripsFromLocalstorage.length > 0 &&
             tripsFromLocalstorage.map(({ city, startDate, endDate }) => (
               <li>
                 <h3>{city}</h3>
@@ -92,10 +117,17 @@ function App() {
                   from {startDate} to {endDate}
                 </p>
               </li>
-            ))}
+            ))} */}
+            {tripsFromLocalstorage.length > 0 &&
+            tripsFromLocalstorage.map(data => <Trip onClick={onTripClick} data={data}/>)}
           <li>
             <button onClick={onAddTripBtnClick}>Add trip</button>
           </li>
+        </ul>
+      </div>
+      <div>
+        <ul>
+        {tripWeatherInfo && tripWeatherInfo.map(info => <DayWeather data={info} />)}
         </ul>
       </div>
       {showModal && (
