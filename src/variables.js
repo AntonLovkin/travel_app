a172e358fd83b8cab9ff6d0f3bb8b5fb1b6deb21
Anton Lovkin <anton28111987@gmail.com>
2024-02-18 19:55:08 +0200

    added variables

---
+++ src/variables.js
@@ -0,0 +1,4 @@
+export const API_KEY = 'XM9YG9VGNME7QHFXDNCCMDYFU';
+export const URL = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline';
+
+// export default {API_KEY, URL}
