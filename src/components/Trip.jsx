f5f584e5cbd6e39122d12a7ea883a25e0841a71e
Anton Lovkin <anton28111987@gmail.com>
2024-02-20 21:46:48 +0200

    added styles

--- src/components/Trip.jsx
+++ src/components/Trip.jsx
@@ -1,14 +1,22 @@
 const Trip = ({onClick, data}) => {
     // console.log(data)
-    const { city, startDate, endDate } = data;
-    return (
-        <li onClick={() => onClick(city, startDate, endDate)} style={{border: "2px solid white", borderRadius: "20px", padding: "10px", margin: "10px", cursor: "pointer"}}>
-                <h3>{city}</h3>
-                <p>
-                  from {startDate} to {endDate}
-                </p>
-              </li>
-    )
+  const { city, startDate, endDate } = data;
+
+  return (
+    <li 
+      style={{
+        display: 'flex', flexDirection: "column", alignItems: 'center',
+        border: "2px solid white", borderRadius: "20px",
+         margin: "10px", cursor: "pointer",
+        width: '250px', height: '150px'
+      }}
+      onClick={() => onClick(city, startDate, endDate)}>
+      <h3>{city}</h3>
+      <p>
+        from {startDate} to {endDate}
+      </p>
+    </li>
+  )
 };
 
 export default Trip;
