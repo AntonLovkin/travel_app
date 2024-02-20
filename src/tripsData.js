df92841931f397e5925c72935628255037144288
Anton Lovkin <anton28111987@gmail.com>
2024-02-20 21:45:12 +0200

    added id

--- src/tripsData.js
+++ src/tripsData.js
@@ -1,4 +1,4 @@
 export const tripsList = [
-  { city: 'Kyiv', startDate: '2024-03-08', endDate: '2024-03-09' },
-  { city: 'Tokio', startDate: '2024-03-06', endDate: '2024-03-09' },
+  { id: 0, city: 'Kyiv', startDate: '2024-03-08', endDate: '2024-03-09' },
+  { id: 1, city: 'Tokio', startDate: '2024-03-06', endDate: '2024-03-09' },
 ];
