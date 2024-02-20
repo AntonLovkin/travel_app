9518d357bd759a9e8612f937776bf3295e6e3386
Anton Lovkin <anton28111987@gmail.com>
2024-02-20 21:46:20 +0200

    added styles and cleared names

--- src/components/TripList.jsx
+++ src/components/TripList.jsx
@@ -1,36 +1,43 @@
 import { useState, useEffect } from 'react';
 
 import Trip from "./Trip"
+
 import { tripsList } from "../tripsData";
 
-function TripList({onTripClick, onAddTripBtnClick}) {
-    const [tripsFromLocalstorage, setTripsFromLocalstorage] = useState(
+function TripList({ onTripClick, showAddModal }) {
+    const [trips, setTrips] = useState(
         JSON.parse(localStorage.getItem('trips')) || tripsList || []
     );
 
     useEffect(() => {
         const trips = JSON.parse(localStorage.getItem('trips'));
-        // console.log(trips);
         if (trips) {
-            setTripsFromLocalstorage(trips);
+            setTrips(trips);
         }
     }, []);
 
-    useEffect(() => {
-        // console.log(tripsFromLocalstorage);
-        localStorage.setItem('trips', JSON.stringify(tripsFromLocalstorage));
-    }, [tripsFromLocalstorage]);
-    
-    
+  useEffect(() => {
+    localStorage.setItem('trips', JSON.stringify(trips));
+  }, [trips]);
+
     return (
         <div>
             <h2>List of Trips</h2>
-            <ul>
-                {tripsFromLocalstorage.length > 0 &&
-                    tripsFromLocalstorage.map(data => <Trip onClick={onTripClick} data={data} />)}
-                <li>
-                    <button onClick={onAddTripBtnClick}>Add trip</button>
-                </li>
+            <ul style={{ display: "flex", flexWrap: "wrap" }}>
+                {trips.length > 0 &&
+                    trips.map(trip =>
+                        <Trip onClick={onTripClick} data={trip} />)}
+                <li><button
+                    style={{
+                        border: "2px solid white", borderRadius: "20px",
+                        padding: "10px", margin: "10px", cursor: "pointer",
+                        width: '250px', height: '150px'
+                    }}
+                    onClick={showAddModal}
+                >
+                    <style>{`button:hover {background-color: tomato;}`}</style>
+                    Add trip
+                </button></li>
             </ul>
         </div>
     );
