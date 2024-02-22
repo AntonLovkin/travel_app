7d168dd15dfaa074f49ddd40a2d8b4ac29bbd6b4
Anton Lovkin <anton28111987@gmail.com>
2024-02-22 22:05:28 +0200

    commit

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,17 @@
 import Modal from "./Modal";
 import Trip from "./Trip";
+import SearchTrip from "./SearchTrip";
+import TripList from "./TripList";
+import DayWeather from "./DayWeather";
+import TodaysWeather from "./TodaysWeather";
+import Timer from "./Timer";
 
 export default {
-  Modal, Trip
-}
+  Modal,
+  Trip,
+  SearchTrip,
+  TripList,
+  DayWeather,
+  TodaysWeather,
+  Timer
+};
